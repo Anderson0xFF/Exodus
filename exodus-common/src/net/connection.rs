620fd15951970c6e92350873e28a17360ad42140
Anderson Almeida <andersonvsc1998@gmail.com>
2023-12-06 20:11:21 +0000

    refactory connection

--- exodus-common/src/net/connection.rs
+++ exodus-common/src/net/connection.rs
@@ -1,6 +1,7 @@
 use std::{os::unix::net::UnixStream, io::{Read, Write}};
 use exodus_errors::ErrorKind;
-use crate::{net::network_message::NetworkMessage, if_, consts::EXODUS_RESPONSE_TIMEOUT_MILLIS};
+use exodus_protocols::network_message::NetworkMessage;
+use crate::if_;
 
 
 #[derive(Debug)]
@@ -22,11 +23,10 @@ impl Connection {
 
     pub fn connect(path: &str) -> Result<Self, ErrorKind> {
         if let Ok(socket) = UnixStream::connect(path) {
-            socket.set_read_timeout(Some(std::time::Duration::from_millis(EXODUS_RESPONSE_TIMEOUT_MILLIS))).unwrap_or_default();
             return Ok(Self::new(socket));
         }
 
-        Err(ErrorKind::EXODUS_CONNECTION_FAILED)
+        Err(ErrorKind::CONNECTION_FAILED)
     }
 
     pub fn set_nonblocking(&mut self, nonblocking: bool) {
@@ -34,14 +34,14 @@ impl Connection {
     }
 
     #[inline]
-    pub fn get_id(&self) -> u32 { self.id }
+    pub fn id(&self) -> u32 { self.id }
 
     pub fn buffer(&mut self) -> Result<Option<NetworkMessage>, ErrorKind> {
         let mut msg = NetworkMessage::default();
 
         match self.socket.read(msg.get_buffer()) {
             Ok(size) => if_!(size > 0, return Ok(Some(msg))),
-            Err(_) => return Err(ErrorKind::EXODUS_CONNECTION_CLOSED),
+            Err(_) => return Err(ErrorKind::CONNECTION_CLOSED),
         }
 
         Ok(None)
