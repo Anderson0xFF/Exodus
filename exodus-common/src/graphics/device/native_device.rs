000e309eb3ef98a04a922dcd49a5d32024a5bdec
Anderson Almeida <andersonvsc1998@gmail.com>
2023-12-08 23:24:32 +0000

    rename

--- exodus-common/src/graphics/device/native_device.rs
+++ exodus-common/src/graphics/device/native_device.rs
@@ -2,35 +2,48 @@ use exodus_errors::ErrorKind;
 use gbm::gbm_create_device;
 use std::sync::Arc;
 
-pub type NativeDeviceRef = Arc<NativeDevice>;
+use crate::{debug, error};
+
+use super::GPUID;
+
+pub type DeviceRef = Arc<Device>;
 pub type NativeBufferRaw = *mut gbm::gbm_bo;
 pub type NativeSurfaceRaw = *mut gbm::gbm_surface;
 
 
 #[derive(Debug)]
-pub struct NativeDevice {
+pub struct Device {
+    id : GPUID,
     device: *mut gbm::gbm_device,
 }
 
-impl NativeDevice {
-    pub fn new(id: i32) -> Result<NativeDeviceRef, ErrorKind> {
+impl Device {
+    pub fn new(id: GPUID) -> Result<DeviceRef, ErrorKind> {
+        debug!("Creating DeviceManager. - GPUID: {}", id);
+
         let device = unsafe {
             gbm_create_device(id)
         };
 
         if device.is_null() {
-            return Err(ErrorKind::NATIVE_DEVICE_CREATE_FAILED);
+            error!("Failed to create gbm_device. - GPUID: {}", id);
+            return Err(ErrorKind::DEVICE_MANAGER_CREATE_FAILED);
         }
 
-        Ok(NativeDeviceRef::new(NativeDevice {
+        Ok(DeviceRef::new(Device {
             device,
+            id,
         }))
     }
 
     pub(crate) fn as_ptr(&self) -> *mut gbm::gbm_device {
         self.device
     }
+
+    pub fn id(&self) -> i32 {
+        self.id
+    }
 }
 
-unsafe impl Send for NativeDevice {}
-unsafe impl Sync for NativeDevice {}
+unsafe impl Send for Device {}
+unsafe impl Sync for Device {}
