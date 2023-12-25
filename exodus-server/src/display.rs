18ec33f2b30e35969481ae3c9f146cb6020fcc84
Anderson Almeida <andersonvsc1998@gmail.com>
2023-12-25 23:19:25 +0000

    refactory display

--- exodus-server/src/display.rs
+++ exodus-server/src/display.rs
@@ -1,42 +1,45 @@
-use exodus_common::{
-    consts::*,
-    graphics::device::GPU,
-    net::connection::Connection,
-    *,
-};
-
+use exodus_common::{consts::{EXODUS_DIRECTORY, EXODUS_LOG}, logger, info, net::connection::Connection, error, debug, memory::Allocator};
 use exodus_errors::ErrorKind;
 use std::{os::unix::net::UnixListener, path};
-use crate::client::Entity;
+use crate::{client::Entity, device::GPU};
 
 #[derive(Debug)]
 pub struct Display {
     id:         i32,
-    gpus:       Vec<GPU>,
     listener:   UnixListener,
+    gpus:       Vec<GPU>,
+    allocator:  Allocator,
 }
 
 impl Display {
 
-    pub fn new() -> Result<Self, ErrorKind> {
+    pub fn new(cache: usize) -> Result<Self, ErrorKind> {
         if !path::Path::new(EXODUS_DIRECTORY).exists() {
             std::fs::create_dir_all(EXODUS_DIRECTORY).unwrap();
-            std::fs::create_dir_all(EXODUS_LOG_DIRECTORY).unwrap();
         }
 
         let (id, dpy) = Self::discovery_display();
-        if let Err(_) = std::env::var(EXODUS_DISPLAY) {
-            std::env::set_var(EXODUS_DISPLAY, id.to_string());
+
+        let loggerfile = format!("exodus-display-{}.log", id);
+        if let Ok(level) = std::env::var(EXODUS_LOG) {
+            let level = match level.parse::<i32>() {
+                Ok(lvl) => logger::Level::from(lvl),
+                Err(_) => logger::Level::Error,
+            };
+
+            logger::Logger::initialize(level, Some(&loggerfile));
+        } else {
+            logger::Logger::initialize(logger::Level::Info, Some(&loggerfile));
         }
 
-        Self::logger_initialize(id);
 
         info!("Initializing display...");
         let listener: UnixListener = Self::create_display_listener(&dpy)?;
-        let gpus = GPU::search_gpus()?;
+
+        let gpus = GPU::enumerate_gpus()?;
 
         info!("Display initialized successfully.");
-        Ok(Self { id, gpus, listener })
+        Ok(Self { id, listener, allocator: Allocator::with_capacity(cache), gpus })
     }
 
     pub fn accept(&self) -> Option<Entity> {
@@ -51,29 +54,6 @@ impl Display {
 
     pub fn id(&self) -> i32 {
         self.id
-    }
-
-    pub fn gpus(&self) -> &[GPU] {
-        &self.gpus
-    }
-
-    pub fn get_gpu(&mut self, id: i32) -> Option<&mut GPU> {
-        let mut gpu = self.gpus.iter_mut().filter(|gpu| gpu.id() == id);
-        gpu.next()
-    }
-
-    fn logger_initialize(id: i32) {
-        let logger_dir = format!("{}/exodus-{}.log", EXODUS_LOG_DIRECTORY, id);
-        if let Ok(level) = std::env::var(EXODUS_LOG) {
-            let level = match level.parse::<i32>() {
-                Ok(lvl) => logger::Level::from(lvl),
-                Err(_) => logger::Level::Error,
-            };
-
-            logger::Logger::init(level, logger_dir.as_str());
-        } else {
-            logger::Logger::init(logger::Level::Info, logger_dir.as_str());
-        }
     }
 
     fn create_display_listener(path: &str) -> Result<UnixListener, ErrorKind> {
@@ -98,25 +78,35 @@ impl Display {
         }
     }
 
-    fn dispose(&mut self) {
+    pub fn dispose(&mut self) {
         debug!("Disposing display...");
-        if let Ok(id) = std::env::var(EXODUS_DISPLAY) {
-            if id == self.id.to_string() {
-                std::env::remove_var(EXODUS_DISPLAY);
-            }
-        }
 
         let display = format!("{}/exodus-{}", EXODUS_DIRECTORY, self.id);
         if path::Path::new(&display).exists() {
             std::fs::remove_file(&display).unwrap();
         }
 
-        for gpu in self.gpus.iter_mut() {
-            gpu.dispose();
-        }
+        self.gpus.iter_mut().for_each(|gpu| gpu.dispose());
+        
         debug!("Display disposed.");
     }
 
+    pub fn get_gpu(&self, id: i32) -> Option<&GPU> {
+        self.gpus.iter().find(|gpu| gpu.id() == id)
+    }
+
+    pub fn get_gpu_mut(&mut self, id: i32) -> Option<&mut GPU> {
+        self.gpus.iter_mut().find(|gpu| gpu.id() == id)
+    }
+
+    pub fn gpus(&self) -> &[GPU] {
+        self.gpus.as_ref()
+    }
+
+    pub fn gpus_mut(&mut self) -> &mut Vec<GPU> {
+        &mut self.gpus
+    }
+    
 }
 
 impl Drop for Display {
